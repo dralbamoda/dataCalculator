237b6b1cfe5a8511092d0ec4c0b5485ae64c8279
Nick Fletcher <nfletcher@canibuild.com>
2023-04-18 08:43:59 +1000

    Try adding linting and formatting

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dataCalculator
+
 data prediction calculator
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let currentValues = {
-	web: 300,
-	social: 200,
-	emails: 250,
-	music: 1000,
-	video: 5000,
-	tv: 100000,
-	apps: 500
+  web: 300,
+  social: 200,
+  emails: 250,
+  music: 1000,
+  video: 5000,
+  tv: 100000,
+  apps: 500,
 };
 const cpg = 0.5;
 const platformFee = 1.99;
@@ -18,72 +18,78 @@ const totalPrice = document.querySelector("#total-price");
 const hero = document.querySelector("#hero");
 
 const mockDataLists = () => {
-	const lists = document.querySelectorAll("datalist");
-	lists.forEach((list) => {
-		const mockList = document.createElement("div");
-		mockList.classList.add("calculator__datalist-mock");
-		const data = list.children;
-		Object.values(data).forEach((o) => {
-			const opt = document.createElement("div");
-			opt.textContent = o.label;
-			mockList.appendChild(opt);
-		});
-		list.parentElement.appendChild(mockList);
-	});
+  const lists = document.querySelectorAll("datalist");
+  lists.forEach((list) => {
+    const mockList = document.createElement("div");
+    mockList.classList.add("calculator__datalist-mock");
+    const data = list.children;
+    Object.values(data).forEach((o) => {
+      const opt = document.createElement("div");
+      opt.textContent = o.label;
+      mockList.appendChild(opt);
+    });
+    list.parentElement.appendChild(mockList);
+  });
 };
 
 const setRanges = (event) => {
-	const id = event.target.id;
-	const thisValue = parseInt(event.target.value);
-	currentValues[id] = thisValue;
-	setValue();
+  const id = event.target.id;
+  const thisValue = parseInt(event.target.value);
+  currentValues[id] = thisValue;
+  setValue();
 };
 
-const getCost =  (dataInGB) => {
-	const textPrice = textValues
-		? parseFloat(Object.values(textValues).find((v) => v.checked).value)
-		: 0;
-	const costPerGig = dataInGB * cpg;
-	return costPerGig + textPrice + platformFee;
-}
+const getCost = (dataInGB) => {
+  const textPrice = textValues
+    ? parseFloat(Object.values(textValues).find((v) => v.checked).value)
+    : 0;
+  const costPerGig = dataInGB * cpg;
+  return costPerGig + textPrice + platformFee;
+};
 
 const getDegrees = (dataInGB) => {
-	return ((dataInGB) / 239.75) * 360;
-}
+  return (dataInGB / 239.75) * 360;
+};
 
 const setValue = (animate = false) => {
-	const currentValue = Object.values(currentValues).reduce(
-		(acc, v) => acc + v,
-		0
-	);
-	const dataInGB = currentValue / 1000;
-	collector.textContent = `${dataInGB.toFixed(2)}GB`;
-	hero.textContent = `${dataInGB.toFixed(2)}`;
-	const degrees = getDegrees(dataInGB);
-	topGraph.style.setProperty("--amount", `${degrees > 358 ? 358 : degrees}deg`);
-	totalPrice.textContent = `${getCost(dataInGB).toFixed(2)}`;
-	const optimiser = optimiserValues
-		? Object.values(optimiserValues).find((v) => v.checked)?.value || 0
-		: 0;
-	
-	let total = 0;
-	if (animate) {
-		const animatedInterval = setInterval(() => {
-			if (total < dataInGB) {
-				collector.textContent = `${(total).toFixed(2)}GB`;
-				totalPrice.textContent = `${getCost(total).toFixed(2)}`;
-				hero.textContent = `${total.toFixed(2)}`;
-				topGraph.style.setProperty("--amount", `${getDegrees(total) > 358 ? 358 : getDegrees(total)}deg`);
-				total++;
-			} else {
-				clearInterval(animatedInterval);
-				collector.textContent = `${dataInGB.toFixed(2)}GB`;
-				totalPrice.textContent = `${getCost(dataInGB).toFixed(2)}`;
-				hero.textContent = `${dataInGB.toFixed(2)}`;
-				topGraph.style.setProperty("--amount", `${degrees > 358 ? 358 : degrees}deg`);
-			}
-		}, 1);
-	}
+  const currentValue = Object.values(currentValues).reduce(
+    (acc, v) => acc + v,
+    0
+  );
+  const dataInGB = currentValue / 1000;
+  collector.textContent = `${dataInGB.toFixed(2)}GB`;
+  hero.textContent = `${dataInGB.toFixed(2)}`;
+  const degrees = getDegrees(dataInGB);
+  topGraph.style.setProperty("--amount", `${degrees > 358 ? 358 : degrees}deg`);
+  totalPrice.textContent = `${getCost(dataInGB).toFixed(2)}`;
+  const optimiser = optimiserValues
+    ? Object.values(optimiserValues).find((v) => v.checked)?.value || 0
+    : 0;
+
+  let total = 0;
+  if (animate) {
+    const animatedInterval = setInterval(() => {
+      if (total < dataInGB) {
+        collector.textContent = `${total.toFixed(2)}GB`;
+        totalPrice.textContent = `${getCost(total).toFixed(2)}`;
+        hero.textContent = `${total.toFixed(2)}`;
+        topGraph.style.setProperty(
+          "--amount",
+          `${getDegrees(total) > 358 ? 358 : getDegrees(total)}deg`
+        );
+        total++;
+      } else {
+        clearInterval(animatedInterval);
+        collector.textContent = `${dataInGB.toFixed(2)}GB`;
+        totalPrice.textContent = `${getCost(dataInGB).toFixed(2)}`;
+        hero.textContent = `${dataInGB.toFixed(2)}`;
+        topGraph.style.setProperty(
+          "--amount",
+          `${degrees > 358 ? 358 : degrees}deg`
+        );
+      }
+    }, 1);
+  }
 };
 
 values.forEach((v) => v.addEventListener("input", setRanges));
